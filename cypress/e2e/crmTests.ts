8949834b83259ce8aa55f027454fcfdc541b7114
justincost <justin_cost@hotmail.com>
2024-09-05 14:19:39 -0400

    Added the rest of API test to validation structure

--- cypress/e2e/crmTests.ts
+++ cypress/e2e/crmTests.ts
@@ -1,5 +1,6 @@
-import { mockResponses } from "../fixtures/mocks/mockResponses"
-import { loginPage } from "../fixtures/pageObjects/loginPage.pageObjects"
+import { mockResponses } from '../fixtures/mocks/mockResponses'
+import { loginPage } from '../fixtures/pageObjects/loginPage.pageObjects'
+import { ContactsEndpoints } from '../fixtures/apiEndpoints/Contacts'
 
 describe('ContactListTests', () => {
     it('Login and verify url on default contact list page.', () => {
@@ -7,10 +8,67 @@ describe('ContactListTests', () => {
       loginPage.visit()
       cy.url().should('include', '/myCRMApp/tabs/Contacts')
     })
-    it.only('Login via API and hit the getAllContacts API endpoint', () =>{
+    it('Login via API and hit the getAllContacts API endpoint and validate field values and types', () =>{
+      const idRegex = /^[A-Za-z0-9]{20}$/
+      const phoneRegex = /^\d{10}$/
+      const dueDateRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{6}Z$/
+
       cy.login('admin')
-      cy.requestWithToken({url: 'https://crmservices.azurewebsites.net/api/Contacts/getAllContacts?status=Contacts&sortBy=fullName&order=ASC'}).then((response) => {
-        cy.log(JSON.stringify(response.body))
+      cy.requestWithToken({url: ContactsEndpoints.getAllContactsPath}).then((response) => {
+        //cy.log(JSON.stringify(response.body))
+        expect(response.body.message).to.equal('200')
+
+        response.body.contacts.forEach((contact) => {
+
+        expect(contact.id).to.match(idRegex)
+        cy.wrap(contact.id).should('match', idRegex);
+        expect(typeof contact.id).to.equal('string')
+        cy.wrap(contact.id).should('be.a', 'string')
+        
+        expect(contact.status).to.equal('Contacts')
+        expect(typeof contact.status).to.equal('string')
+        cy.wrap(contact.status).should('be.a', 'string')
+        
+        expect(contact.fullName).to.equal('Justin Case')
+        expect(typeof contact.fullName).to.equal('string')
+        cy.wrap(contact.fullName).should('be.a', 'string')
+        
+        expect(contact.phoneNumber).to.match(phoneRegex)
+        cy.wrap(contact.phoneNumber).should('match', phoneRegex);
+        expect(typeof contact.phoneNumber).to.equal('string')
+        cy.wrap(contact.phoneNumber).should('be.a', 'string')
+        
+        expect(typeof contact.email).to.equal('string')
+        cy.wrap(contact.email).should('be.a', 'string')
+        
+        expect(typeof contact.background).to.equal('string')
+        cy.wrap(contact.background).should('be.a', 'string')
+        
+        expect(typeof contact.score).to.equal('number')
+        cy.wrap(contact.score).should('be.a', 'number')
+        
+        if(contact.actions.length > 0) {
+          contact.actions.forEach((action) => {
+            expect(typeof action.completed).to.equal('boolean')
+            cy.wrap(action.completed).should('be.a', 'boolean')
+            
+            expect(action.dueDate).to.match(dueDateRegex)
+            cy.wrap(action.dueDate).should('match', dueDateRegex);
+            expect(typeof action.dueDate).to.equal('string')
+            cy.wrap(action.dueDate).should('be.a', 'string')
+            
+            expect(typeof action.actionDescription).to.equal('string')
+            cy.wrap(action.actionDescription).should('be.a', 'string')
+            
+            expect(action.actionType).to.be.null
+            cy.wrap(action.actionType).should('equal', null)
+            
+            expect(action.formattedDueDate).to.be.null
+            cy.wrap(action.formattedDueDate).should('equal', null)
+          })
+        }
+        
+      })
       })
     })
     it('Mock 30 entries in getAllContacts api endpoint', () => {
---
+++ cypress/fixtures/apiEndpoints/Contacts.ts
@@ -0,0 +1,4 @@
+export module ContactsEndpoints {
+    export const getAllContactsFullPath = 'https://crmservices.azurewebsites.net/api/Contacts/getAllContacts?status=Contacts&sortBy=fullName&order=ASC'
+    export const getAllContactsPath = 'https://crmservices.azurewebsites.net/api/Contacts/getAllContacts?status=Contacts'
+}
